656c570728cf9d4c3446c21e3444786c692c3f7f
sushil <vangarsushil@gmail.com>
2020-11-10 17:57:32 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import {Component} from '@angular/core';
+
+import {
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
